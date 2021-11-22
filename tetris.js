91eb1a3afd66f1fe8bf2e0fe5166d2a3ea571e2a
Priya Tiwari <92025463+uc-prt12@users.noreply.github.com>
2021-11-22 12:12:20 +0530

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -57,7 +57,7 @@ function randomPiece() {
   let r = (randomN = Math.floor(Math.random() * PIECES.length)); // 0 -> 6
   return new Piece(PIECES[r][0], PIECES[r][1]);
 }
-let pieceshift = randomPiece();
+let pieceShift = randomPiece();
 // The Object Piece
 function Piece(tetromino, color) {
   this.tetromino = tetromino;
@@ -96,7 +96,7 @@ Piece.prototype.moveDown = function () {
   } else {
     // we lock the piece and generate a new one
     this.lock();
-    pieceshift = randomPiece();
+    pieceShift = randomPiece();
     for (i = 9; i >= 0; i--) {
       let c = 0;
       if (
@@ -326,13 +326,13 @@ Piece.prototype.collision = function (x, y, piece) {
 document.addEventListener("keydown", CONTROL);
 function CONTROL(event) {
   if (event.keyCode == 37) {
-    pieceshift.moveLeft();
+    pieceShift.moveLeft();
     dropStart = Date.now();
   } else if (event.keyCode == 39) {
-    pieceshift.moveRight();
+    pieceShift.moveRight();
     dropStart = Date.now();
   } else if (event.keyCode == 40) {
-    pieceshift.moveDown();
+    pieceShift.moveDown();
   }
 }
 // drop the piece every 1sec
@@ -342,10 +342,10 @@ function drop() {
   let now = Date.now();
   let delta = now - dropStart;
   if (delta > 1000) {
-    pieceshift.moveDown();
-    dropStart = Date.now() + 7000;
+    pieceShift.moveDown();
+    dropStart = Date.now() + 1000;
     if (level.value == 2) {
-      dropStart = Date.now() + 1000;
+      dropStart = Date.now() + 300;
     } else if (level.value == 3) {
       dropStart = Date.now() - 300;
     } else if (level.value == 4) {
@@ -359,15 +359,15 @@ function drop() {
 drop();
 // For suggested buttons
 function leftmove() {
-  pieceshift.moveLeft();
+  pieceShift.moveLeft();
   dropStart = Date.now();
 }
 function rightmove() {
-  pieceshift.moveRight();
+  pieceShift.moveRight();
   dropStart = Date.now();
 }
 function downmove() {
-  pieceshift.moveDown();
+  pieceShift.moveDown();
 }
 
 // -----------------  End  Here ------------------------------ //
